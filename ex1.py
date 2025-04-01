d5cc580bbaa6cf2401475adbbbe4c7e743c20351
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-01 16:03:47 -0300

    Commit final: implementação da classe Circle

--- ex1.py
+++ ex1.py
@@ -6,16 +6,26 @@ class Circle:
         self.raio = raio
 
     def area(self):
-
         return math.pi * self.raio ** 2
+
     def perimetro(self):
-        
         return 2 * math.pi * self.raio
-     
+
     def test_pertencente(self, ponto):
-        
         x_ponto, y_ponto = ponto
         x_centro, y_centro = self.centro
 
         distancia = math.sqrt((x_ponto - x_centro) ** 2 + (y_ponto - y_centro) ** 2)
+
+        print(f"Distância do ponto {ponto} ao centro {self.centro}: {distancia}")
+
         return distancia <= self.raio
+
+
+
+c1 = Circle((2, 2), 4)
+
+
+print(f"Ponto (0, -1) pertence ao círculo? {c1.test_pertencente((0, -1))}")
+
+    
